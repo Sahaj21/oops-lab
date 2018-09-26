7196585a51e9a16e7c7a824d725442a5319a46da
Sahaj Kulshrestha <kulshresthasahaj@gmail.com>
2018-09-26 19:46:35 +0530

    odd even using or

---
+++ odd even.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{ int a;
+
+cout<<"Enter the value you want to check->"<<endl;
+cin>>a;
+
+if((a&1)==0)
+cout<<endl<<"It is even";
+else
+cout<<endl<<"It is odd";
+
+	
+	
+	
+	return 0;
+	
+}

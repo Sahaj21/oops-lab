429f960591d8581f5a19be4d0dbb27c00b90fd57
SAURABH SHEKHAR <42865533+SaurabhShekhar161@users.noreply.github.com>
2018-10-13 13:21:20 +0530

    Update multiplymatrix.cpp

--- multiplymatrix.cpp
+++ multiplymatrix.cpp
@@ -7,39 +7,39 @@ int main()
 	
 	cin>>n;
 	
-	int a[n][n],b[n][n],c[n][n],sum=0;
+	int a[n][n],b[n][n],c[n][n],sum=0,i,j,p;
 	cout<<"\nEnter value in 1st matrix\n";
-	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
 		{
 			cout<<"Enter vaue for index "<<i<<" "<<j;
 			cin>>a[i][j];
 		}
 	cout<<"\n";
  	cout<<"Enter value in 2nd matrix\n";
-	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
+	for(i=0;i<n;i++)
+		for( j=0;j<n;j++)
 		{
 			cout<<"Enter vaue for index "<<i<<" "<<j;			
 			cin>>b[i][j];
 		}
 	cout<<"\n";
-	for(int p=0;p<n;p++)
-	for(int i=0;i<n;i++)
+	for(p=0;p<n;p++)
+	for( i=0;i<n;i++)
 	{    
 	
 	sum=0;
-		for(int j=0;j<n;j++)
+		for(j=0;j<n;j++)
 			sum=sum+a[i][j]*b[j][i]	;
 	c[p][i]=sum;
 	}
 	cout<<"\n";
 	cout<<"The resulted matrix is:\n";
-		for(int i=0;i<n;i++)
+		for(i=0;i<n;i++)
 		{
 		cout<<"\n";
 		
-		for(int j=0;j<n;j++)
+		for( j=0;j<n;j++)
 			cout<<c[i][j]<<" ";
 	    }
 	
